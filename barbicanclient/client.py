46a611c24d2a899b58b9768c5ca05a96475bff1e
Arash Ghoreyshi <arashghoreyshi@gmail.com>
2013-06-28 16:00:31 -0500

    Fixed a bug involving secret expiration dates

--- barbicanclient/client.py
+++ barbicanclient/client.py
@@ -199,8 +199,7 @@ class Connection(object):
         secret_dict['algorithm'] = algorithm
         secret_dict['cypher_type'] = cypher_type
         secret_dict['bit_length'] = bit_length
-        if expiration is not None:
-            secret_dict['expiration'] = parse_isotime(expiration)
+        secret_dict['expiration'] = expiration
         self._remove_empty_keys(secret_dict)
         LOG.debug(_("Request body: {0}").format(secret_dict))
         hdrs, body = self._perform_http(href=href,
