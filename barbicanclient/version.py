a66681bf4d4e74103e941dd265886df2e79817f9
Donald Stufft <donald@stufft.io>
2014-01-03 11:23:38 -0500

    Migrate to pbr
    Change-Id: Id914183a5098e2fdc3f325fc83ecc5161886733d

--- barbicanclient/version.py
+++ barbicanclient/version.py
@@ -17,5 +17,7 @@
 Cloudkeep's Barbican Client version
 """
 
-__version__ = '1.0.0'
-__version_info__ = tuple(__version__.split('.'))
+import pbr.version
+
+version_info = pbr.version.VersionInfo("barbicanclient")
+__version__ = version_info.version_string()
--- setup.py
+++ setup.py
@@ -15,65 +15,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
+# THIS FILE IS MANAGED BY THE GLOBAL REQUIREMENTS REPO - DO NOT EDIT
 import setuptools
 
-
-name = 'python-barbicanclient'
-
-
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
-def get_version():
-    PKG = "barbicanclient"
-    VERSIONFILE = os.path.join(PKG, "version.py")
-    version = "unknown"
-    try:
-        version_file = open(VERSIONFILE, "r")
-        for line in version_file:
-            if '__version__' in line:
-                version = line.split("'")[1]
-                break
-    except EnvironmentError:
-        pass  # Okay, there is no version file.
-    return version
-
 setuptools.setup(
-    name=name,
-    version=get_version(),
-    description='Client Library for OpenStack Barbican Key Management API',
-    long_description=read('README.md'),
-    keywords="openstack encryption key-management secret",
-    url='https://github.com/cloudkeep/barbican',
-    license='Apache License (2.0)',
-    author='Rackspace, Inc.',
-    author_email='openstack-dev@lists.openstack.org',
-    packages=setuptools.find_packages(
-        exclude=['tests', 'tests.*', 'examples', 'examples.*']
-    ),
-    install_requires=[
-        'argparse>=1.2.1',
-        'eventlet>=0.13.0',
-        'requests>=1.2.3',
-        'python-keystoneclient>=0.3.2',
-    ],
-    test_suite='nose.collector',
-    tests_require=['nose'],
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Console',
-        'Environment :: OpenStack',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Information Technology',
-        'License :: OSI Approved :: Apache Software License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2.7',
-        'Environment :: No Input/Output (Daemon)',
-    ],
-    entry_points={
-        "console_scripts": [
-            "barbican = barbicanclient.barbican:main",
-        ],
-    },
+    setup_requires=['pbr'],
+    pbr=True,
 )
