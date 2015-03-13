93ae60629d770951836f4bd52b4e9e71d70779aa
Douglas Mendizábal <mail@doug.gt>
2015-03-13 15:20:18 -0500

    Don't use tempest log
    Tempest has been updated to oslo_log so tempest.openstack.common.log is
    no longer available.
    
    Change-Id: I6163d5b2e0690fe6ff056f0bbd294552667ba8a6

--- functionaltests/client/base.py
+++ functionaltests/client/base.py
@@ -13,6 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
+import logging
 import os
 
 import oslotest.base as oslotest
@@ -20,7 +21,6 @@ from barbicanclient import client
 from keystoneclient.auth import identity
 from keystoneclient import session
 from tempest import config
-from tempest.openstack.common import log as logging
 
 CONF = config.CONF
 
