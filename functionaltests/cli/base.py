4676b09afe49a03a6540cdd8cee3c669945ac81d
Steve Heyman <steve.heyman@rackspace.com>
2015-04-27 13:04:33 -0500

    Add Secret CLI smoke tests
    Added secret CLI tests as well as utility functions for parsing
    command line output for easier testing.
    
    Change-Id: Ifeed8ce3b31f1bd24c710a4999c8c616948e1a90

--- functionaltests/cli/base.py
+++ functionaltests/cli/base.py
@@ -15,10 +15,15 @@ limitations under the License.
 """
 
 import exceptions as exc
+import re
 import six
+
 
 from functionaltests.base import BaseTestCase
 from barbicanclient import barbican
+from tempest import config
+
+CONF = config.CONF
 
 
 class CmdLineTestCase(BaseTestCase):
@@ -28,6 +33,42 @@ class CmdLineTestCase(BaseTestCase):
         super(CmdLineTestCase, self).setUp()
 
         self.cmdline_client = barbican.Barbican()
+
+    def add_auth_and_endpoint(self, arg_list):
+        """ Update an argument list with authentication and endpoint data
+
+        Keystone v3 introduced the concept of a domain, so only the v3
+        flavor will include domain names.
+
+        Keystone v3 changed "tenant" to "project" so the v3 flavor uses
+        the term 'project' in its args while v2 uses 'tenant'.
+
+        Both v2 and v2 require the auth URL, userid/password, and barbican
+        endpoint URL.
+
+        :param arg_list: the current argument list
+        :return: the argument list is updated with the authentication and
+        endpoint args
+        """
+        if 'v3' in CONF.identity.auth_version.lower():
+            arg_list.extend(['--os-auth-url', CONF.identity.uri_v3])
+            arg_list.extend(['--os-project-name', CONF.keymanager.project_name])
+            # NOTE(jaosorior): Should we add the user_domain_name to the
+            #                  config?
+            arg_list.extend(
+                ['--os-user-domain-name', CONF.keymanager.project_domain_name])
+            arg_list.extend(
+                ['--os-project-domain-name',
+                 CONF.keymanager.project_domain_name])
+        else:
+            arg_list.extend(['--os-auth-url', CONF.identity.auth_uri])
+            arg_list.extend(['--os-tenant-name', CONF.keymanager.project_name])
+
+        arg_list.extend(['--os-username', CONF.keymanager.username,
+                         '--os-password', CONF.keymanager.password])
+
+        arg_list.extend(['--endpoint', CONF.keymanager.url])
+        self.LOG.info('updated command string: %s', arg_list)
 
     def issue_barbican_command(self, argv):
         """ Issue the barbican command and return its output.
@@ -55,3 +96,52 @@ class CmdLineTestCase(BaseTestCase):
         errstr = self.cmdline_client.stderr.getvalue()
 
         return outstr, errstr
+
+    def _prettytable_to_secret(self, str):
+        """ Create a dict from the values in a PrettyTable string.
+
+        :param str: a string representing a PrettyTable output from a
+        barbican secret store or get command.
+        :return: a dict containing the fields and values from the output.
+        """
+        retval = {}
+        if str is not None and len(str) > 0:
+            table_body = re.split('\+-*\+-*\+\n', str)[2:-1]
+            lines = table_body[0].split('\n')
+            for line in lines:
+                if len(line) > 0:
+                    row = line.split('|')
+                    key = row[1].strip()
+                    value = row[2].strip()
+                    retval[key] = value
+        return retval
+
+    def _prettytable_to_secret_list(self, str):
+        """ Create a list from the values in a PrettyTable string.
+
+        :param str: a string representing a PrettyTable output from a
+        barbican secret list command.
+        :return: a list containing one dict for each column in the table.
+        If there are no entries then an empty list will be returned.
+        """
+        retval = []
+        if str is not None and len(str) > 0:
+            rows = re.findall('\|(.*)?\n', str)
+            # Remove header
+            rows.pop(0)
+            for row in rows:
+                # Parse out the entire row
+                (href, name, created, status, content_types, algorithm,
+                 bit_length, mode, expiration) = re.findall('\s*(.*?)\s*\|',
+                                                            row)
+                entry_dict = {'Secret href': href,
+                              'Name': name,
+                              'Created': created,
+                              'Status': status,
+                              'Content types': content_types,
+                              'Algorithm': algorithm,
+                              'Bit length': bit_length,
+                              'Mode': mode,
+                              'Expiration': expiration}
+                retval.append(entry_dict)
+        return retval
