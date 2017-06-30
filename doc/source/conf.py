4eed5c84cd1fc59ade488f7103361e44f57b9c0f
Van Hung Pham <hungpv@vn.fujitsu.com>
2017-06-30 10:25:24 +0700

    Switch from oslosphinx to openstackdocstheme
    As part of the docs migration work[0] for Pike we need to switch to use
    the openstackdocstheme.
    
    [0]https://review.openstack.org/#/c/472275/
    
    Change-Id: I81e959fbd1544d543dd39898dbc2d63ef991a925

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -22,7 +22,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 extensions = [
     'sphinx.ext.autodoc',
     #'sphinx.ext.intersphinx',
-    'oslosphinx'
+    'openstackdocstheme',
 ]
 
 # autodoc generation is a bit aggressive and a nuisance when doing heavy
@@ -58,9 +58,15 @@ pygments_style = 'sphinx'
 # html_theme = '_theme'
 # html_static_path = ['static']
 html_theme_options = {}
+html_theme = 'openstackdocs'
 
 # Output file base name for HTML help builder.
 htmlhelp_basename = '%sdoc' % project
+
+# If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
+# using the given strftime format.
+# html_last_updated_fmt = '%b %d, %Y'
+html_last_updated_fmt = '%Y-%m-%d %H:%M'
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass
@@ -74,3 +80,8 @@ latex_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 #intersphinx_mapping = {'http://docs.python.org/': None}
+
+# -- Options for openstackdocstheme -------------------------------------------
+repository_name = 'openstack/python-barbicanclient'
+bug_project = 'python-barbicanclient'
+bug_tag = ''
